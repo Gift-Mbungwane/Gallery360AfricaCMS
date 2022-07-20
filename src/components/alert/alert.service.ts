4a92e47c432c4c7816a35352907c595ea1a6e5c4
Gift-Mbungwane <Gift.mbungwane01@outlook.com>
2022-07-20 16:35:25 +0200

    fixed deployment error

--- src/components/alert/alert.service.ts
+++ src/components/alert/alert.service.ts
@@ -1,48 +1,48 @@
-import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { Alert } from './alert.model';
-import { AlertSettings } from './alert-settings';
+// import { Injectable } from '@angular/core';
+// import { Observable, BehaviorSubject } from 'rxjs';
+// import { filter } from 'rxjs/operators';
+// import { Alert } from './alert.model';
+// import { AlertSettings } from './alert-settings';
 
-@Injectable({ providedIn: 'root' })
-export class AlertService {
-  private subject = new BehaviorSubject<Alert>(null);
-  private defaultId = 'default-alert';
+// @Injectable({ providedIn: 'root' })
+// export class AlertService {
+//   //private subject = new BehaviorSubject<Alert>();
+//   private defaultId = 'default-alert';
 
-  onAlert(id = this.defaultId): Observable<Alert> {
-    return this.subject.asObservable().pipe(filter((x) => x && x.id === id));
-  }
+//   onAlert(id = this.defaultId): Observable<Alert> {
+//     return this.subject.asObservable().pipe(filter((x) => x && x.id === id));
+//   }
 
-  success(message: string, options?: any) {
-    this.alert(
-      new Alert({ ...options, alertType: AlertSettings.SUCCESS, message })
-    );
-  }
+//   success(message: string, options?: any) {
+//     this.alert(
+//       new Alert({ ...options, alertType: AlertSettings.SUCCESS, message })
+//     );
+//   }
 
-  error(message: string, options?: any) {
-    this.alert(
-      new Alert({ ...options, alertType: AlertSettings.ERROR, message })
-    );
-  }
+//   error(message: string, options?: any) {
+//     this.alert(
+//       new Alert({ ...options, alertType: AlertSettings.ERROR, message })
+//     );
+//   }
 
-  info(message: string, options?: any) {
-    this.alert(
-      new Alert({ ...options, alertType: AlertSettings.INFO, message })
-    );
-  }
+//   info(message: string, options?: any) {
+//     this.alert(
+//       new Alert({ ...options, alertType: AlertSettings.INFO, message })
+//     );
+//   }
 
-  warn(message: string, options?: any) {
-    this.alert(
-      new Alert({ ...options, alertType: AlertSettings.WARNING, message })
-    );
-  }
+//   warn(message: string, options?: any) {
+//     this.alert(
+//       new Alert({ ...options, alertType: AlertSettings.WARNING, message })
+//     );
+//   }
 
-  alert(alert: Alert) {
-    alert.id = alert.id || this.defaultId;
-    this.subject.next(alert);
-  }
+//   alert(alert: Alert) {
+//     alert.id = alert.id || this.defaultId;
+//     this.subject.next(alert);
+//   }
 
-  clear(id = this.defaultId) {
-    this.subject.next(new Alert({ id }));
-  }
-}
+//   clear(id = this.defaultId) {
+//     this.subject.next(new Alert({ id }));
+//   }
+// }
